ff4c8a653b1ebbb90fedee08bfa4714ab8b61dfb
your_github_sasy <saaga.syrjanen@hotmail.com>
2026-05-11 19:50:24 +0200

    fixed coordinates to be accurate

--- app/src/main/java/dk/itu/adventurequest/LocationData.kt
+++ app/src/main/java/dk/itu/adventurequest/LocationData.kt
@@ -4,12 +4,12 @@ import com.google.android.gms.maps.model.LatLng
 
 object LocationData {
     val discoveryPoints = listOf(
-        PinItem(LatLng(55.7949, 12.5714), "Eremitagen", "Advanced Route - Nature Trails", "Conversation"),
-        PinItem(LatLng(55.6811, 12.5404), "Landbohøjskolens Have", "Accessible Route - Flat & Paved", "Conversation"),
-        PinItem(LatLng(55.7005, 12.5448), "Superkilen Park", "Accessible Route - Flat & Paved", "Conversation"),
-        PinItem(LatLng(55.6418, 12.6543), "Kastrup Søbad", "Accessible Route - Wooden Decking", "Conversation"),
-        PinItem(LatLng(55.6534, 12.5932), "Grønjordssøen Udkigspunkt", "Advanced Route - Uneven Ground", "Conversation"),
-        PinItem(LatLng(55.6111, 12.5256), "Kalvebod Fælled Tower", "Advanced Route - Nature Trails", "Conversation"),
-        PinItem(LatLng(55.6433, 12.5516), "Naturpark Amager", "Accessible Route - Flat Paths", "Conversation")
+        PinItem(LatLng(55.7952042, 12.5714407), "Eremitagen", "Advanced Route - Nature Trails", "Conversation"),
+        PinItem(LatLng(55.6807852, 12.5421746), "Landbohøjskolens Have", "Accessible Route - Flat & Paved", "Conversation"),
+        PinItem(LatLng(55.7013424, 12.5426383), "Superkilen Park", "Accessible Route - Flat & Paved", "Conversation"),
+        PinItem(LatLng(55.6454401, 12.6494404), "Kastrup Søbad", "Accessible Route - Wooden Decking", "Conversation"),
+        PinItem(LatLng(55.650633, 12.585011), "Grønjordssøen Udkigspunkt", "Advanced Route - Uneven Ground", "Conversation"),
+        PinItem(LatLng(55.620216, 12.567670), "Kalvebod Fælled Tower", "Advanced Route - Nature Trails", "Conversation"),
+        PinItem(LatLng(55.6442784, 12.5552548), "Naturpark Amager - Havneslusen Nord", "Accessible Route - Flat Paths", "Conversation")
     )
 }
